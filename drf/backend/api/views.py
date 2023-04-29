3e0e47e4d46c3a12fd8b58664d63f2162945bcad
Stefannus Christian <schristian38@students.calvin.ac.id>
2023-04-30 03:25:26 +0700

    django model instance as API response

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,14 +1,29 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    body = request.body # byte string of JSON Data
+    # <-- GET ECHO DATA SECTION -->
+    # body = request.body # byte string of JSON Data
+    # data = {}
+    # try:
+    #     data = json.loads(body) # tsring of json data --> python dict
+    # except: pass
+    # data['params'] = dict(request.GET)
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
+    # print(data)
+    # <-- GET ECHO DATA SECTION -->
+
+    # <-- Django Model Instance as API Response -->
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # tsring of json data --> python dict
-    except: pass
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    print(data)
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # model instance (model_data)
+        # turn a Python dict
+        # return JSON to my client
     return JsonResponse(data)
