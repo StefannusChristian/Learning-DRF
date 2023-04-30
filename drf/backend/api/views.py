6754cbd0384544d01bb3ad07d9412ca13a62a64b
Stefannus Christian <schristian38@students.calvin.ac.id>
2023-04-30 18:17:15 +0700

    Rest Framework View & Response & Django Rest Framework Model Serializers

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -2,7 +2,11 @@ import json
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     # <-- GET ECHO DATA SECTION -->
     # body = request.body # byte string of JSON Data
@@ -16,20 +20,25 @@ def api_home(request, *args, **kwargs):
     # print(data)
     # <-- GET ECHO DATA SECTION -->
 
+    """
+    DRF API VIEW
+    """
+
     # <-- Django Model Instance as API Response -->
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
-        # model instance (model_data)
+    if instance:
+        # data['id'] = instance.id
+        # data['title'] = instance.title
+        # data['content'] = instance.content
+        # data['price'] = instance.price
+        # model instance (instance)
         # turn a Python dict
         # return JSON to my client
     # <-- Django Model Instance as API Response -->
 
    # <-- Django Model Instance to Dictionary -->
-        data = model_to_dict(model_data, fields=['id', 'title'])
+        # data = model_to_dict(instance, fields=['id', 'title','sale_price'])
+        data = ProductSerializer(instance).data
 
     return JsonResponse(data)
